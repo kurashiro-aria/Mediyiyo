aa0ac474b27a8944a3c21e993216cc48b76a2072
kurashiro-aria <eduardo.rojas.a96@gmail.com>
2026-09-25 14:27:26 -0300

    Fix JVM compatibility and broaden Android support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,24 @@ plugins {
 android {
     namespace = "cl.kura.mediyiyo"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "cl.kura.mediyiyo"
-        minSdk = 26
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures { compose = true }
 }
 
